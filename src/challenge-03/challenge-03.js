8424d7b3394480b4ba1d606fd428ee31168aae47
Javier Piqueras <javierpiquerasmartinez@gmail.com>
2024-12-26 08:54:33 +0100

    Challenge03 - Instruction (bis)

---
+++ src/challenge-03/challenge-03.js
@@ -0,0 +1,8 @@
+/**
+ * @param {{ name: string, quantity: number, category: string }[]} inventory
+ * @returns {object} The organized inventory
+ */
+function organizeInventory(inventory) {
+  // Code here
+  return {}
+}
